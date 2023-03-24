de3eae5280339ffe54ed8fa05a04c2b3bd888769
KalenT <123968809+KGTCreator@users.noreply.github.com>
2023-03-24 14:43:34 +0000

    Major Change To Everything

---
+++ script.js
@@ -0,0 +1,23 @@
+function clearName(){
+	if (document.getElementById('inputFirstName3').value == 'First and Last Name'){
+	document.getElementById('inputFirstName3').value = '';
+	} 
+}
+
+function resetName(){
+	if (document.getElementById('inputFirstName3').value == ''){
+	document.getElementById('inputFirstName3').value = 'First and Last Name';
+	} 
+}
+
+function clearPhone(){
+	if (document.getElementById('inputLastName3').value == 'Wakeman'){
+	document.getElementById('inputLastName3').value = '';
+	} 
+}
+
+function resetPhone(){
+	if (document.getElementById('inputLastName3').value == ''){
+	document.getElementById('inputLastName3').value = 'Wakeman';
+	} 
+}
